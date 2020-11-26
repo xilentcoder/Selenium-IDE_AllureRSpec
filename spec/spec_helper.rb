e2eb663c4489aba673954d6a21fa57c457d058f0
xilentcoder <72712316+xilentcoder@users.noreply.github.com>
2020-11-27 03:40:35 +0800

    seleniumIDE with rspec
    test suite using seleniumIDE with rspec and allure for reporting

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'allure-rspec'
+require 'uuid'
+
+AllureRspec.configure do |c|
+  c.results_directory = 'report/allure-results'
+  c.clean_results_directory = true
+  #c.link_tms_pattern = 'https://example.org/tms/{}'
+  #c.link_issue_pattern = 'https://example.org/issue/{}'
+end
+
